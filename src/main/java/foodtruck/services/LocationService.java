4611da01e61a43a8dedab8d9b5f6912100c28028
Andrew Violette <aviolette@divvydose.com>
2020-07-28 12:00:19 -0500

    Added findByAlias

--- src/main/java/foodtruck/services/LocationService.java
+++ src/main/java/foodtruck/services/LocationService.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 import foodtruck.models.Location;
 
 public interface LocationService {
+
   Optional<Location> findById(long id);
 
   Optional<Location> findByName(String name);
+
+  Optional<Location> findByAlias(String alias);
 }
