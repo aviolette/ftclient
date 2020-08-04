cc642955cfffa84463cb270955b9b27cd6ce9af5
Andrew Violette <aviolette@divvydose.com>
2020-08-04 05:57:11 -0500

    add convenience function to Clocks

--- src/main/java/foodtruck/util/Clocks.java
+++ src/main/java/foodtruck/util/Clocks.java
@@ -1,7 +1,13 @@
 package foodtruck.util;
 
+import java.time.Clock;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class Clocks {
   public static final ZoneId CHICAGO = ZoneId.of("America/Chicago");
+
+  public static final ZonedDateTime now(Clock clock) {
+    return ZonedDateTime.ofInstant(clock.instant(), clock.getZone());
+  }
 }
